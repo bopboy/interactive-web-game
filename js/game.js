f5561f67c4d6084b7a455001663e9775721404a7
bopboy <39391891+bopboy@users.noreply.github.com>
2022-07-06 20:14:48 +0900

    03 게임 기본 설정 및 방향키 이벤트 세팅

---
+++ js/game.js
@@ -0,0 +1,26 @@
+const key = {
+    keyDown: {},
+    keyValue: {
+        37: 'left',
+        39: 'right',
+        38: 'up'
+    }
+}
+const windowEvent = () => {
+    window.addEventListener('keydown', e => {
+        key.keyDown[key.keyValue[e.which]] = true
+        console.log(key.keyDown)
+    })
+    window.addEventListener('keyup', e => {
+        key.keyDown[key.keyValue[e.which]] = false
+        console.log(key.keyDown)
+    })
+}
+
+const init = () => {
+    windowEvent()
+}
+
+window.onload = () => {
+    init()
+}

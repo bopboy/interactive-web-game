5944580b45958258db4f15c3b059b84b6bec3b9a
bopboy <39391891+bopboy@users.noreply.github.com>
2022-07-10 09:53:22 +0900

    49 NPC 모달에 퀘스트 내용 추가

--- js/class.js
+++ js/class.js
@@ -384,10 +384,25 @@ class Npc {
     talk() {
         if (!this.talkOn && this.npcCrash) {
             this.talkOn = true
+            this.quest()
             this.modal.classList.add('active')
         } else if (this.talkOn) {
             this.talkOn = false
             this.modal.classList.remove('active')
         }
+    }
+    quest() {
+        let text = ''
+        text += `
+            <figure class="npc_img">
+                <img src="./lib/images/npc.png">
+            </figure>
+            <p>
+                마을에 몬스터가 출몰해 주민들을 좀비로 만들고 있다. 몬스터를 사냥해서 주민을 구하고
+                <span>레벨을 5 이상</span>으로 만들어 힘을 증명한다면 좀비왕을 물리칠 수 있도록 내 힘을 빌려줄께!
+            </p>
+        `
+        const modalInner = document.querySelector('.quest_modal .inner_box .quest_talk')
+        modalInner.innerHTML = text
     }
 }
